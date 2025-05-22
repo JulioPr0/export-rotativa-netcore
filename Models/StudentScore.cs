8c846c3da53eb0a23f719d6686527c8586b11b03
julpramaitama <julio.pramaitama@binus.ac.id>
2025-05-22 11:00:39 +0700

    feat: model with properties definition

---
+++ Models/StudentScore.cs
@@ -0,0 +1,19 @@
+public class StudentScore
+{
+    public int Id
+    {
+        get; set;
+    }
+    public string Name
+    {
+        get; set;
+    }
+    public int Score
+    {
+        get; set;
+    }
+    public int Rank
+    {
+        get; set;
+    }
+}
